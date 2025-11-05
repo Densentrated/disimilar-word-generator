a9a84b1de39ce98b907ddeaaed52b319e01bc3d3
D-108 <therealdensentrated@gmail.com>
2025-11-05 10:59:21 -0500

    Reset Project From Scratch

--- view_list.py
+++
@@ -1,8 +0,0 @@
-import json
-import pandas as pd
-
-df = pd.DataFrame(
-    json.load(open("top1000_distant_closest.json")),
-    columns=["vietnamese", "closest_english", "cosine"],
-)
-print(df.head(20))
